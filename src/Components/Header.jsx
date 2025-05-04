111ae9b637656fec26f53138f52e70f623aed8b5
AKToran <aktoran60@gmail.com>
2025-05-04 17:38:18 +0600

    theme customization and date fns

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format } from 'date-fns';
 
 const Header = () => {
   return (
     <div className='flex justify-center flex-col items-center gap-2'>
-      <img src={logo} alt="" />
-      <p className='text-xl text-gray-600 font-light'>Journalism without fear or favor.</p>
+      <img className='w-[400px]' src={logo} alt="" />
+      <p className='text-xl text-accent font-light'>Journalism without fear or favor.</p>
+      <p className='font-semibold text-accent'>{format(new Date(), "EEEE, MM MMMM, yyyy")}</p>
     </div>
   );
 };
